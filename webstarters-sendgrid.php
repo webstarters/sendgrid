3e35ee617ef0d8e3afeddce5d400c1f531d84bd4
Peter Jørgensen <pj@webstarters.dk>
2019-05-24 13:44:34 +0200

    Fix version number

--- webstarters-sendgrid.php
+++ webstarters-sendgrid.php
@@ -4,7 +4,7 @@
     Plugin Name: Webstarters SendGrid
     Plugin URI: https://webstarters.dk
     Description: A plugin from Webstarters for integration with SendGrid
-    Version: 1.0.0
+    Version: 0.1.0
     Author: Webstarters
     Author URI: https://webstarters.dk
 */
